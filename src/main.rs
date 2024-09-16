7d412cdcfdc1d4ae20585d467150f2396af6bbfe
cocoman99 <cocoman99@o2.pl>
2024-09-16 23:12:18 +0200

    Matching

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("+++ Guess THE Number +++");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-    
+    //println!("The secret number is: {secret_number}");
+
     println!("Input your guess: ");
 
     let mut guess = String::new();
@@ -15,5 +16,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read the line"); // handling potential failure
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("YOU guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("YOU WIN!"),
+    }
 }
